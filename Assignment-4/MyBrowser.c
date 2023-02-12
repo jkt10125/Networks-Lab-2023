0f3c74ecf4d23abef1fb8a6deaa3dead96bdaeee
abhay-k47 <akabhay9685@gmail.com>
2023-02-12 18:14:58 +0530

    added sendRequest, sendFile, recvFile

--- Assignment-4/MyBrowser.c
+++ Assignment-4/MyBrowser.c
@@ -9,70 +9,84 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
 
-#define IP "127.0.0.1"
-#ifndef PORT
-#define PORT 20000
-#endif
 #define BUFLEN 50
 
-enum CONTENT_TYPE
+enum FILE_TYPE
 {
     HTML,
     PDF,
     JPG,
     OTHER
 };
-
-int getExtension(char *url)
+enum READ_STATUS
 {
-    char *ext = strrchr(url, '.');
+    INVALID,
+    NONE,
+    QUIT,
+    OK
+};
+
+int getExtension(char *filename)
+{
+    char *ext = strrchr(filename, '.');
     if (ext == NULL)
         return OTHER;
     if (strcmp(ext, ".html") == 0)
         return HTML;
-    if (strcmp(ext + 1, ".pdf"))
+    if (strcmp(ext, ".pdf") == 0)
         return PDF;
-    if (strcmp(ext + 1, ".jpg"))
+    if (strcmp(ext, ".jpg") == 0)
         return JPG;
     return OTHER;
 }
 
-
-int readRequest(char *command, char *host, char *url, int* port_p, char *filepath)
+int readRequest(char *command, char *host, char *url, int *port_p, char *filepath)
 {
-    // command = (char *)malloc(5 * sizeof(char));
-    // host = (char *)malloc(16 * sizeof(char));
-    // url = (char *)malloc(1024 * sizeof(char));
-    scanf("%[^ ]s", command);
-    perror(command);
+    command[0] = 0;
+    host[0] = 0;
+    url[0] = 0;
+    filepath[0] = 0;
+
+    char *input = NULL;
+    size_t size = 0;
+    int n = getline(&input, &size, stdin);
+    while (input[n - 1] == ' ' || input[n - 1] == '\t' || input[n - 1] == '\n')
+        input[--n] = 0;
+    while (input[0] == ' ' || input[0] == '\t')
+        input++;
+
+    sscanf(input, "%s http://%[^/]%s %s", command, host, url, filepath);
+
+    // printf("Command: %s\n", command);
+    // printf("Host: %s\n", host);
+    // printf("URL: %s\n", url);
+    // printf("Filename: %s\n", filepath);
+
+    if (command[0] == 0)
+        return NONE;
+
     if (strcmp(command, "QUIT") == 0)
     {
-        return -1;
+        if (strcmp(input, "QUIT"))
+            return INVALID;
+        return QUIT;
     }
-    getchar();
-    scanf("http://%[^/]s", host);
-    perror(host);
-    scanf("%s", url);
-    perror(url);
-    *port_p = 80;
-    for (int i = 0; url[i]; i++)
-    {
-        if (url[i] == ':')
-        {
-            *port_p = atoi(url + i + 1);
-            url[i] = 0;
-            break;
-        }
-    }
-    printf("%d\n", *port_p);
-    // if(strcmp(command, "PUT")==0){
-    //     getchar();
-    //     scanf("%s", filepath);
-    //     getchar();
-    // }
+    if (strcmp(command, "GET") && strcmp(command, "PUT"))
+        return INVALID;
+    if (host[0] == 0 || url[0] == 0)
+        return INVALID;
+    if ((strcmp(command, "GET") == 0) ^ (filepath[0] == 0))
+        return INVALID;
 
-    return 0;
+    strcpy(url, strtok(url, ":"));
+    char *temp = strtok(NULL, ":");
+    if (temp == NULL)
+        *port_p = 80;
+    else
+        *port_p = atoi(temp);
+    return OK;
 }
 
 int getConnection(char *host, int port)
@@ -81,20 +95,16 @@ int getConnection(char *host, int port)
     struct sockaddr_in serv_addr;
 
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        perror("socket() failed");
         return -1;
-    }
+
+    // printf("%s, %d\n", host, port);
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(PORT);
+    inet_aton(host, &serv_addr.sin_addr);
+    serv_addr.sin_port = htons(port);
 
     if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        perror("connect() failed");
         return -1;
-    }
 
     return sockfd;
 }
@@ -104,88 +114,152 @@ char *getRequest(char *command, char *host, char *url, char *filepath)
     time_t rawtime = time(NULL);
     char *request = (char *)malloc(4096);
     if (strcmp(command, "GET") == 0)
-    {   
-        printf("GET\n");
-        sprintf(request+strlen(request), "%s %s %s\n", command, url, "HTTP/1.1");
-        printf("%s\n", request);
-        sprintf(request+strlen(request), "%s: %s\n", "Host", host);
+    {
+        sprintf(request + strlen(request), "%s %s %s\n", command, url, "HTTP/1.1");
 
-        sprintf(request+strlen(request), "%s: ", "Accept");
+        sprintf(request + strlen(request), "%s: %s\n", "Host", host);
+
+        sprintf(request + strlen(request), "%s: ", "Accept");
         switch (getExtension(url))
         {
         case HTML:
-            sprintf(request+strlen(request), "%s\n", "text/html");
+            sprintf(request + strlen(request), "%s\n", "text/html");
             break;
         case PDF:
-            sprintf(request+strlen(request), "%s\n", "application/pdf");
+            sprintf(request + strlen(request), "%s\n", "application/pdf");
             break;
         case JPG:
-            sprintf(request+strlen(request), "%s\n", "image/jpeg");
+            sprintf(request + strlen(request), "%s\n", "image/jpeg");
             break;
         case OTHER:
-            sprintf(request+strlen(request), "%s\n", "text/*");
-            break;
-        default:
+            sprintf(request + strlen(request), "%s\n", "text/*");
             break;
         }
-        sprintf(request+strlen(request), "%s: %s\n", "Accept-Language", "en-US, en;q=0.9");
 
-        sprintf(request+strlen(request), "%s: ", "Date");
-        strftime(request+strlen(request), 30, "%a, %d %b %Y %H:%M:%S GMT", gmtime(&rawtime));
-        sprintf(request+strlen(request), "\n");
+        sprintf(request + strlen(request), "%s: %s\n", "Accept-Language", "en-US, en;q=0.9");
+
+        sprintf(request + strlen(request), "%s: ", "Date");
+        strftime(request + strlen(request), 30, "%a, %d %b %Y %H:%M:%S GMT", gmtime(&rawtime));
+        sprintf(request + strlen(request), "\n");
 
         rawtime -= 2 * 24 * 60 * 60;
-        sprintf(request+strlen(request), "%s: ", "If-modified-since");
-        strftime(request+strlen(request), 30, "%a, %d %b %Y %H:%M:%S GMT", gmtime(&rawtime));
-        sprintf(request+strlen(request), "\n");
+        sprintf(request + strlen(request), "%s: ", "If-modified-since");
+        strftime(request + strlen(request), 30, "%a, %d %b %Y %H:%M:%S GMT", gmtime(&rawtime));
+        sprintf(request + strlen(request), "\n");
 
-        sprintf(request+strlen(request), "%s: %s\n", "Connection", "close");
-        sprintf(request+strlen(request), "\n");
+        sprintf(request + strlen(request), "%s: %s\n", "Connection", "close");
+
+        sprintf(request + strlen(request), "\n");
     }
 
     else if (strcmp(command, "PUT") == 0)
     {
-        printf("GET\n");
-        sprintf(request+strlen(request), "%s %s %s\n", command, url, "HTTP/1.1");     
-        printf("GET\n");
+        sprintf(request + strlen(request), "%s %s %s\n", command, url, "HTTP/1.1");
 
-        sprintf(request+strlen(request), "%s: ", "Date");
-        strftime(request+strlen(request), 30, "%a, %d %b %Y %H:%M:%S GMT", gmtime(&rawtime));
-        sprintf(request+strlen(request), "\n");
+        sprintf(request + strlen(request), "%s: ", "Date");
+        strftime(request + strlen(request), 30, "%a, %d %b %Y %H:%M:%S GMT", gmtime(&rawtime));
+        sprintf(request + strlen(request), "\n");
 
-        sprintf(request+strlen(request), "%s: %s\n", "Content-Language", "en-US");
+        sprintf(request + strlen(request), "%s: %s\n", "Content-Language", "en-US");
+
         FILE *fp = fopen(filepath, "r");
+        if (fp == NULL)
+        {
+            perror("fopen");
+            return request;
+        }
         fseek(fp, 0L, SEEK_END);
-        sprintf(request+strlen(request), "%s: %ld\n", "Content-length", ftell(fp));
+        sprintf(request + strlen(request), "%s: %ld\n", "Content-length", ftell(fp));
         fclose(fp);
-        sprintf(request+strlen(request), "%s: ", "Content-type");
+
+        sprintf(request + strlen(request), "%s: ", "Content-type");
+
         switch (getExtension(filepath))
         {
         case HTML:
-            sprintf(request+strlen(request), "%s\n", "text/html");
+            sprintf(request + strlen(request), "%s\n", "text/html");
             break;
         case PDF:
-            sprintf(request+strlen(request), "%s\n", "application/pdf");
+            sprintf(request + strlen(request), "%s\n", "application/pdf");
             break;
         case JPG:
-            sprintf(request+strlen(request), "%s\n", "image/jpeg");
+            sprintf(request + strlen(request), "%s\n", "image/jpeg");
             break;
         case OTHER:
-            sprintf(request+strlen(request), "%s\n", "text/*");
-            break;
-        default:
+            sprintf(request + strlen(request), "%s\n", "text/*");
             break;
         }
+
         rawtime -= 2 * 24 * 60 * 60;
         struct tm *gtime = gmtime(&rawtime);
-        sprintf(request+strlen(request), "%s: ", "If-modified-since");
-        strftime(request+strlen(request), 30, "%a, %d %b %Y %H:%M:%S GMT", gtime);
-        sprintf(request+strlen(request), "\n");
+        sprintf(request + strlen(request), "%s: ", "If-modified-since");
+        strftime(request + strlen(request), 30, "%a, %d %b %Y %H:%M:%S GMT", gtime);
+        sprintf(request + strlen(request), "\n");
 
-        sprintf(request+strlen(request), "%s: %s", "Connection", "close");
-        sprintf(request+strlen(request), "\n");
+        sprintf(request + strlen(request), "%s: %s\n", "Connection", "close");
+
+        sprintf(request + strlen(request), "\n");
     }
     return request;
+}
+
+// send size_n bytes, return -1 if fails
+int sendRequest(int sockfd, const char *request, int size_n)
+{
+    int i, j;
+    char buffer[BUFLEN];
+
+    i = 0;
+    while (i < size_n)
+    {
+        memset(buffer, 0, BUFLEN);
+        j = 0;
+        while (j < BUFLEN && i < size_n)
+        {
+            buffer[j++] = request[i++];
+        }
+        if (send(sockfd, buffer, j, 0) < 0)
+            return -1;
+    }
+    return 0;
+}
+
+int sendFile(int sockfd, const char *filepath)
+{
+    int fd, n;
+    char buffer[BUFLEN];
+
+    fd = open(filepath, O_RDONLY);
+    if (fd < 0)
+        return -1;
+
+    while ((n = read(fd, buffer, BUFLEN)) > 0)
+    {
+        if (send(sockfd, buffer, n, 0) < 0)
+            return -1;
+    }
+    if (send(sockfd, "", 1, 0) < 0)
+        return -1;
+    close(fd);
+    return 0;
+}
+
+int recvFile(int sockfd, const char *filepath)
+{
+    int fd, n;
+    char buffer[BUFLEN];
+
+    fd = open(filepath, O_WRONLY | O_CREAT);
+    if (fd < 0)
+        return -1;
+
+    while ((n = recv(sockfd, buffer, BUFLEN, 0)) > 0)
+    {
+        if (write(fd, buffer, n) < 0)
+            return -1;
+    }
+    close(fd);
+    return 0;
 }
 
 int main()
@@ -195,24 +269,50 @@ int main()
     {
         char command[5], host[16], url[256], filepath[256], *request, *response;
         int port, sockfd;
+
         printf("MyOwnBrowser>");
-        if (readRequest(command, host, url, &port, filepath) < 0)
-            break;
-        perror(command);
-        perror(host);
-        perror(url);
-        printf("%d\n", port);
-        if(strcmp(command, "GET")==0) perror(filepath);
-        printf("%s %s %s %d %s\n", command, host, url, port, filepath);
-        if ((sockfd = getConnection(host, port)) < 0)
+
+        int status = readRequest(command, host, url, &port, filepath);
+        if (status == INVALID)
         {
-            perror("Cound not connect");
+            printf("Invalid request!\n");
             continue;
         }
+        if (status == NONE)
+            continue;
+        if (status == QUIT)
+            break;
+
+        if ((sockfd = getConnection(host, port)) < 0)
+        {
+            perror("Could not connect to server");
+            continue;
+        }
+
         request = getRequest(command, host, url, filepath);
+
         printf("%s\n", request);
-        // if (sendRequest(sockfd, request) < 0)
-        //     break;
+
+        if (sendRequest(sockfd, request, strlen(request)) < 0)
+        {
+            perror("Could not send request to server");
+            continue;
+        }
+        if (strcmp(command, "PUT") == 0)
+        {
+            if (sendFile(sockfd, filepath) < 0)
+            {
+                perror("Could not send file to server");
+                continue;
+            }
+        }
+        if (strcmp(command, "GET") == 0)
+        {
+            if(recvFile(sockfd, "temp.html")<0){
+                perror("Could not recv file from server");
+                continue;
+            }
+        }
         // if ((response = getResponse(sockfd)) == NULL)
         // {
         //     break;
--- Assignment-4/MyHTTP.c
+++ Assignment-4/MyHTTP.c
@@ -7,39 +7,13 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
 
-#ifndef PORT
-#define PORT 20000
-#endif
 #define MAXCLIENT 5
 #define BUFLEN 50
 
-// send size_n bytes, return -1 if fails
-int sendString(int sockfd, const char *input, int size_n)
-{
-    int i, j;
-    char buffer[BUFLEN];
-
-    i = 0;
-    while (i < size_n)
-    {
-        memset(buffer, 0, BUFLEN);
-        j = 0;
-        while (j < BUFLEN && i < size_n)
-        {
-            buffer[j++] = input[i++];
-        }
-        if (send(sockfd, buffer, j, 0) < 0)
-        {
-            perror("send() failed");
-            return EXIT_FAILURE;
-        }
-    }
-    return EXIT_SUCCESS;
-}
-
 // recv until null-character is recieved, returns NULL if fails
-char *recvString(int sockfd)
+char *recvRequest(int sockfd)
 {
     int i, j, recvlen, capacity = 1;
     char buffer[BUFLEN];
@@ -73,18 +47,62 @@ char *recvString(int sockfd)
         {
             packet[i++] = buffer[j++];
         }
-        if (packet[i - 1] == '\0')
+        if (packet[i - 1] == '\n' && packet[i - 2] == '\n')
         {
             break;
         }
     }
     return packet;
 }
+int sendFile(int sockfd, const char *filepath)
+{
+    int fd, n;
+    char buffer[BUFLEN];
 
-void communicate(int newsockfd){
+    fd = open(filepath, O_RDONLY);
+    if (fd < 0)
+        return -1;
+    printf("%d\n", fd);
+    while ((n = read(fd, buffer, BUFLEN)) > 0)
+    {
+        if (send(sockfd, buffer, n, 0) < 0)
+            return -1;
+    }
+    // if (send(sockfd, "", 1, 0) < 0)
+    //     return -1;
+    close(fd);
+    return 0;
 }
 
-int main()
+void communicate(int newsockfd)
+{
+    char *s = recvRequest(newsockfd);
+    char command[5], url[256];
+    strtok(s, "\n");
+    sscanf(s, "%s %s", command, url);
+    while (1)
+    {
+        char *header = strtok(NULL, "\n");
+        if (header == NULL)
+        {
+            break;
+        }
+        if (header[0] == 0)
+            break;
+        char field[128], value[1024];
+        sscanf(header, "%[^:]: %[^\n]", field, value);
+    }
+    if (strcmp(command, "GET")==0)
+    {
+        if (sendFile(newsockfd, url+1) < 0)
+        {
+            perror("Could not send file to client");
+            return;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
 {
     int sockfd, newsockfd;
     socklen_t clilen;
@@ -100,7 +118,7 @@ int main()
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_port = htons(atoi(argv[1]));
 
     if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
@@ -132,13 +150,14 @@ int main()
 
         pid_t pid = fork();
 
-        if(pid < 0){
+        if (pid < 0)
+        {
             perror("fork() failed");
             close(newsockfd);
             continue;
         }
         // child process
-        if (pid==0)
+        if (pid == 0)
         {
             close(sockfd);
             communicate(newsockfd);
