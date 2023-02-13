8b698716a3f875d5e74256ed1626cc6051bd5d59
jkt10125 <jkt10125@gmail.com>
2023-02-13 12:39:54 +0530

    Added response script for GET

--- Assignment-4/MyHTTP.c
+++ Assignment-4/MyHTTP.c
@@ -4,13 +4,26 @@
 #include <sys/types.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <time.h>
 #include <fcntl.h>
 
 #define MAXCLIENT 5
 #define BUFLEN 50
+
+
+enum FILE_TYPE
+{
+    HTML,
+    PDF,
+    JPG,
+    OTHER
+};
+
+char *generateResponse(char *command, char *filepath);
 
 // recv until null-character is recieved, returns NULL if fails
 char *recvRequest(int sockfd)
@@ -77,9 +90,13 @@ int sendFile(int sockfd, const char *filepath)
 void communicate(int newsockfd)
 {
     char *s = recvRequest(newsockfd);
-    char command[5], url[256];
+
+    printf("%s", s);
+
+    char command[5], url[256], http_version[10];
     strtok(s, "\n");
-    sscanf(s, "%s %s", command, url);
+    sscanf(s, "%s %s %s", command, url, http_version);
+
     while (1)
     {
         char *header = strtok(NULL, "\n");
@@ -92,6 +109,9 @@ void communicate(int newsockfd)
         char field[128], value[1024];
         sscanf(header, "%[^:]: %[^\n]", field, value);
     }
+
+    printf("%s", generateResponse(command, "test.c"));
+
     if (strcmp(command, "GET")==0)
     {
         if (sendFile(newsockfd, url+1) < 0)
@@ -100,6 +120,93 @@ void communicate(int newsockfd)
             return;
         }
     }
+}
+
+int getExtension(char *filename)
+{
+    char *ext = strrchr(filename, '.');
+    if (ext == NULL)
+        return OTHER;
+    if (strcmp(ext, ".html") == 0)
+        return HTML;
+    if (strcmp(ext, ".pdf") == 0)
+        return PDF;
+    if (strcmp(ext, ".jpg") == 0)
+        return JPG;
+    return OTHER;
+}
+
+
+char *getModifiedTime(char *filepath) {
+    struct stat file_stat;
+
+    if (stat(filepath, &file_stat) == -1) {
+        perror("stat");
+        return "-1";
+    }
+
+    return (ctime(&file_stat.st_mtime));
+}
+
+char *generateResponse(char *command, char *filepath) {
+    char *response = strdup("");
+    
+    int status_code = 200;
+    
+    FILE *fp = fopen(filepath, "r");
+    if (fp == NULL) status_code = 404;
+    else fclose(fp);
+
+    sprintf(response + strlen(response), "HTTP/1.1 %d OK\n", status_code);
+
+    time_t rawtime = time(NULL);
+    if (strcmp(command, "GET") == 0) {
+
+        sprintf(response + strlen(response), "%s: ", "Expires");
+        rawtime += 3 * 24 * 60 * 60;
+        strftime(response + strlen(response), 31, "%a, %d %b %Y %H:%M:%S GMT\n", gmtime(&rawtime));
+
+        sprintf(response + strlen(response), "%s: %s\n", "Cache-control", "no-store");
+
+
+        if (status_code == 200) {
+
+            sprintf(response + strlen(response), "%s: %s\n", "Content-language", "en-us");
+            sprintf(response + strlen(response), "%s: ", "Content-type");
+
+            switch (getExtension(filepath))
+            {
+                case HTML:
+                    sprintf(response + strlen(response), "%s\n", "text/html");
+                    break;
+                case PDF:
+                    sprintf(response + strlen(response), "%s\n", "application/pdf");
+                    break;
+                case JPG:
+                    sprintf(response + strlen(response), "%s\n", "image/jpeg");
+                    break;
+                case OTHER:
+                    sprintf(response + strlen(response), "%s\n", "text/*");
+                    break;
+            }
+
+            FILE *fp = fopen(filepath, "r");
+            if (fp == NULL)
+            {
+                perror("fopen");
+                return response;
+            }
+            fseek(fp, 0L, SEEK_END);
+            sprintf(response + strlen(response), "%s: %ld\n", "Content-length", ftell(fp));
+            fclose(fp);
+
+            sprintf(response + strlen(response), "%s: %s\n", "Last-modified", getModifiedTime(filepath));
+        }
+    }
+    else {
+        // do for PUT
+    }
+    return response;
 }
 
 int main(int argc, char *argv[])
